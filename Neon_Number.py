3bdd7d54394b421284334a01f05b013fd2a5fc97
sdpprasad <96641595+sdpprasad@users.noreply.github.com>
2022-05-06 21:16:20 +0530

    n=int(input()) b=n*n s=0 while b!=0:     r=b%10     s+=r     b=b//10 if n==s:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,7 +1,7 @@
 n=int(input())
 b=n*n
 s=0
-while(b!=0):
+while b!=0:
     r=b%10
     s+=r
     b=b//10
@@ -9,3 +9,4 @@ if n==s:
     print("Neon Number")
 else:
     print("Not Neon Number")
+    
