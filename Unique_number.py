d3b7e2a42a2b31cf98ec0e8449d2d4fdc705ab1f
sdpprasad <96641595+sdpprasad@users.noreply.github.com>
2022-07-22 22:32:15 +0530

    a=input() le=len(a) b=[] for i in a:     if i not in b:         b.append(i) le2=len(b) if le==le2:     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,18 +1,11 @@
-n=int(input())
-t=n
-rev=0
-b=' '
-while n>0:
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-    c=str(r)
-    a=list(c)
-    for i in c:
-        if i not in b and c.count(i)==1:
-            b+=i
-d=int(b)
-if(d==rev):
+a=input()
+le=len(a)
+b=[]
+for i in a:
+    if i not in b:
+        b.append(i)
+le2=len(b)
+if le==le2:
     print("Unique Number")
 else:
     print("Not Unique Number")
