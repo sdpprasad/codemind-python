08b429f05b179cfa27f2255e7f4226a690ceda7a
sdpprasad <96641595+sdpprasad@users.noreply.github.com>
2022-07-23 16:04:54 +0530

    n=input() p,s=1,0 for i in n:     s+=int(i)**p     p+=1 if s==int(n):     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,21 +1,9 @@
-n=int(input())
-rev=0
-c=0
-s=0
-i=1
-t=n
-while n>0:
-    re=n%10
-    rev=rev*10+re
-    n=n//10
-while rev>0:
-    r=rev%10
-    rev=rev//10
-    c+=1
-    while i<=c:
-        s+=r**i
-        i+=1
-if s==t:
+n=input()
+p,s=1,0
+for i in n:
+    s+=int(i)**p
+    p+=1
+if s==int(n):
     print(True)
 else:
     print(False)
